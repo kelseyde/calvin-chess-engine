a8dd8a3e3610c970ce832479f9386d0e20081fff
= <=>
2023-10-11 00:53:00 +0200

    Fix repetition bug

--- src/main/java/com/kelseyde/calvin/search/repetition/RepetitionTable.java
+++ src/main/java/com/kelseyde/calvin/search/repetition/RepetitionTable.java
@@ -7,31 +7,28 @@ import java.util.Map;
 
 public class RepetitionTable {
 
-    private final Map<Long, Integer> hashes;
+    private Map<Long, Integer> hashes;
 
     public RepetitionTable(Board board) {
-        hashes = new HashMap<>();
-//        push(board.getGameState().getZobristKey());
-        board.getGameStateHistory().forEach(gameState -> push(gameState.getZobristKey()));
+        init(board);
     }
 
-    public void push(Board board) {
-        long hash = board.getGameState().getZobristKey();
-        hashes.compute(hash, (key, value) -> value == null ? 1 : ++value);
+    public void init(Board board) {
+        hashes = new HashMap<>();
+        push(board.getGameState().getZobristKey());
+        board.getGameStateHistory().forEach(gameState -> push(gameState.getZobristKey()));
     }
 
     public void push(long hash) {
         hashes.compute(hash, (key, value) -> value == null ? 1 : ++value);
     }
 
-    public void pop(Board board) {
-        long hash = board.getGameState().getZobristKey();
+    public void pop(long hash) {
         hashes.computeIfPresent(hash, (key, value) -> --value);
     }
 
-    public boolean isThreefoldRepitition(Board board) {
-        long hash = board.getGameState().getZobristKey();
-        return hashes.getOrDefault(hash, 0) == 2;
+    public boolean isThreefoldRepetition(long hash) {
+        return hashes.getOrDefault(hash, 0) == 3;
     }
 
 }

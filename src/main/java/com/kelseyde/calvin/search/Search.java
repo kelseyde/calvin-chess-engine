5c3a53e7c5001a928b6aeaef154232fa615bf875
= <=>
2023-10-16 17:42:22 +0200

    Add javadoc

--- src/main/java/com/kelseyde/calvin/search/Search.java
+++ src/main/java/com/kelseyde/calvin/search/Search.java
@@ -2,6 +2,10 @@ package com.kelseyde.calvin.search;
 
 import java.time.Duration;
 
+/**
+ * Search for the best move/evaluation (encapsulated in a {@link SearchResult}) within a give time limit.
+ * See {@link Searcher} for a concrete implementation, using an iterative deepening approach.
+ */
 public interface Search {
 
     SearchResult search(Duration duration);

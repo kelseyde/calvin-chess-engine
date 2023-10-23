3f4ff9c5c41ab3fa91ae72f73feb3cd73065d3a2
= <=>
2023-10-23 16:42:42 +0200

    Add sperft tests

---
+++ src/test/java/com/kelseyde/calvin/movegeneration/sperft/StartingPositionSPerftTest.java
@@ -0,0 +1,24 @@
+package com.kelseyde.calvin.movegeneration.sperft;
+
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+
+public class StartingPositionSPerftTest extends SPerftTest {
+
+    @Getter
+    private final String fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+    @Getter
+    private final String subFolder = "starting_position";
+
+    @Test
+    public void testDepthFive() {
+        sPerft(5);
+    }
+
+    @Test
+    public void testDepthTen() {
+        sPerft(10);
+    }
+
+}

d7c1bc111472d89d7463ac067b1b111960df09d7
kelseyde <daniel.kelsey@hotmail.co.uk>
2023-11-25 10:22:35 +0100

    WIP

--- src/test/java/com/kelseyde/calvin/movegeneration/sperft/SPerftTest.java
+++ src/test/java/com/kelseyde/calvin/movegeneration/sperft/SPerftTest.java
@@ -2,6 +2,7 @@ package com.kelseyde.calvin.movegeneration.sperft;
 
 import com.kelseyde.calvin.board.Board;
 import com.kelseyde.calvin.search.Searcher;
+import com.kelseyde.calvin.tuning.copy.Searcher2;
 import com.kelseyde.calvin.utils.notation.FEN;
 
 import java.io.IOException;

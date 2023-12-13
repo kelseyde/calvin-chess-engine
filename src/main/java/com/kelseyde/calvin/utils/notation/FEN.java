61906dec4f04ee52402ddc98248de02c64e96170
kelseyde <daniel.kelsey@hotmail.co.uk>
2023-12-13 16:54:46 +0100

    Optimize imports

--- src/main/java/com/kelseyde/calvin/utils/notation/FEN.java
+++ src/main/java/com/kelseyde/calvin/utils/notation/FEN.java
@@ -1,7 +1,6 @@
 package com.kelseyde.calvin.utils.notation;
 
 import com.kelseyde.calvin.board.Board;
-import com.kelseyde.calvin.board.Piece;
 import com.kelseyde.calvin.board.Zobrist;
 import com.kelseyde.calvin.utils.BoardUtils;
 
--- src/test/java/com/kelseyde/calvin/puzzles/TacticsTest.java
+++ src/test/java/com/kelseyde/calvin/puzzles/TacticsTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
-import java.util.Collections;
 
 @Disabled
 public class TacticsTest {

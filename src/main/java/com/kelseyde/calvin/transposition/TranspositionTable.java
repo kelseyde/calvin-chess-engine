d27be7669cfe57e08d7cf4a8ca8a2188cdb9b34f
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-05-08 21:26:22 +0200

    Piece values + PST tuning = ~14 elo

--- src/main/java/com/kelseyde/calvin/transposition/TranspositionTable.java
+++ src/main/java/com/kelseyde/calvin/transposition/TranspositionTable.java
@@ -138,7 +138,7 @@ public class TranspositionTable {
         long fill = Arrays.stream(entries).filter(Objects::nonNull).count();
         float fillPercentage = ((float) fill / (float) tableSize) * 100;
         float hitPercentage = ((float) hits / (float) tries) * 100;
-        System.out.printf("TT %s -- size: %s / %s (%s), tries: %s, hits: %s (%s)%n", this.hashCode(), fill, entries.length, fillPercentage, tries, hits, hitPercentage);
+        //System.out.printf("TT %s -- size: %s / %s (%s), tries: %s, hits: %s (%s)%n", this.hashCode(), fill, entries.length, fillPercentage, tries, hits, hitPercentage);
     }
 
 }

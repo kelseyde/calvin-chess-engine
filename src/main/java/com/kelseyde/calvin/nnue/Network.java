c75ce0643ab660caf4682cc6a0096cd9f56e0967
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-05-29 01:18:49 +0200

    WIP

---
+++ src/main/java/com/kelseyde/calvin/nnue/Network.java
@@ -0,0 +1,24 @@
+package com.kelseyde.calvin.nnue;
+
+/**
+ * This class serves as a work-in-progress playground where I jot down all my notes related to neural networks and NNUE.
+ * There will come a day when it turns into a fully-fledged NNUE to relace my hand-crafted evaluation function.
+ * But not this day...
+ */
+public class Network {
+
+    /*
+
+    *** WHAT IS NNUE? ***
+
+    NNUE (ƎUИИ Efficiently Updatable Neural Network) is a neural network architecture that takes advantage of the fact
+    that, in games like chess, a single move does not change the state of the board that much. Therefore, minimal changes
+    are required to update the network inputs between evaluations.
+
+
+
+
+
+     */
+
+}

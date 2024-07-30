1e7acffff92d4f59e078aa54936fad2a79a0d34a
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-07-30 12:36:20 +0200

    Add 4.0.2 release artifacts to /bin

--- bin/calvin-4.0.1.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java --add-modules jdk.incubator.vector -jar calvin-chess-engine-4.0.1.jar
---
+++ bin/calvin-4.0.2.bat
@@ -0,0 +1,2 @@
+@echo off
+java --add-modules jdk.incubator.vector -jar calvin-chess-engine-4.0.2.jar

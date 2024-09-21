c0dd3f3c19833ae2c7ceaf7d2866b7bd9270ec17
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-09-21 17:51:28 +0200

    Optimize imports

--- src/test/java/com/kelseyde/calvin/evaluation/SEETest.java
+++ src/test/java/com/kelseyde/calvin/evaluation/SEETest.java
@@ -2,8 +2,8 @@ package com.kelseyde.calvin.evaluation;
 
 import com.kelseyde.calvin.board.Board;
 import com.kelseyde.calvin.board.Move;
-import com.kelseyde.calvin.utils.notation.FEN;
 import com.kelseyde.calvin.search.moveordering.SEE;
+import com.kelseyde.calvin.utils.notation.FEN;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 

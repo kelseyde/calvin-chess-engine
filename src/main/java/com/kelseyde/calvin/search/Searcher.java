5fb01ceacb45e5f3b004c12ced142d9610348b25
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-09-28 08:24:01 +0200

    WIP

--- src/main/java/com/kelseyde/calvin/search/Searcher.java
+++ src/main/java/com/kelseyde/calvin/search/Searcher.java
@@ -230,7 +230,7 @@ public class Searcher implements Search {
         // If the position has not been searched yet, the search will be potentially expensive. So let's search with a
         // reduced depth expecting to record a move that we can use later for a full-depth search.
         if (!rootNode
-//                && !excluded
+                && !excluded
                 && !inCheck
                 && (ttEntry == null || ttEntry.getMove() == null)
                 && ply > 0
@@ -254,7 +254,7 @@ public class Searcher implements Search {
         // Pre-move-loop pruning: If the static eval indicates a fail-high or fail-low, there are several heuristic we
         // can employ to prune the node and its entire subtree, without searching any moves.
 //        if (!pvNode && !inCheck && !excluded) {
-        if (!pvNode && !inCheck) {
+        if (!pvNode && !inCheck && !excluded) {
 
             // Reverse Futility Pruning - https://www.chessprogramming.org/Reverse_Futility_Pruning
             // If the static evaluation + some significant margin is still above beta, then let's assume this position
@@ -315,6 +315,7 @@ public class Searcher implements Search {
             // If the static evaluation + some margin is still < alpha, and the current move is not interesting (checks,
             // captures, promotions), then let's assume it will fail low and prune this node.
             if (!pvNode
+                && !excluded
                 && depth <= config.fpDepth.value
                 && staticEval + config.fpMargin.value + depth * config.fpScale.value <= alpha
                 && !inCheck

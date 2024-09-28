89cc3dd17f5100798f963c32dc77eb85e4d33c36
Dan Kelsey <daniel.kelsey@hotmail.co.uk>
2024-09-28 20:07:21 +0200

    Optimise MovePicker (#129)
    * Optimise MovePicker
    
    * Optimise checker, attack and check calculation
    
    * Revert move ordering changes

--- src/main/java/com/kelseyde/calvin/search/picker/QuiescentMovePicker.java
+++ src/main/java/com/kelseyde/calvin/search/picker/QuiescentMovePicker.java
@@ -24,8 +24,9 @@ public class QuiescentMovePicker extends MovePicker {
         while (nextMove == null) {
             nextMove = switch (stage) {
                 case TT_MOVE -> pickTTMove();
-                case NOISY -> pickMove(filter, Stage.END);
-                case QUIET, END -> null;
+                case GEN_NOISY -> generate(filter, Stage.NOISY);
+                case NOISY -> pickMove(Stage.END);
+                case GEN_QUIET, QUIET, END -> null;
             };
             if (stage == Stage.END) break;
         }

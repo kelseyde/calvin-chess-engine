e5078aa601bd14937d28bab5fc6ef183811dcc3d
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-10-16 11:57:11 +0200

    Seprate LMR table for noisies

--- src/main/java/com/kelseyde/calvin/engine/EngineConfig.java
+++ src/main/java/com/kelseyde/calvin/engine/EngineConfig.java
@@ -44,6 +44,8 @@ public class EngineConfig {
     public final Tunable lmrDepth             = new Tunable("LmrDepth", 2, 0, 8, 1);
     public final Tunable lmrBase              = new Tunable("LmrBase", 90, 50, 100, 5);
     public final Tunable lmrDivisor           = new Tunable("LmrDivisor", 310, 200, 400, 10);
+    public final Tunable lmrCapBase           = new Tunable("LmrCapBase", 90, 50, 100, 5);
+    public final Tunable lmrCapDivisor        = new Tunable("LmrCapDivisor", 310, 200, 400, 10);
     public final Tunable lmrMinMoves          = new Tunable("LmrMinMoves", 3, 2, 5, 1);
     public final Tunable lmrMinPvMoves        = new Tunable("LmrMinPvMoves", 4, 2, 5, 1);
     public final Tunable lmpDepth             = new Tunable("LmpDepth", 4, 0, 8, 1);
@@ -75,17 +77,17 @@ public class EngineConfig {
     public final Tunable nodeTmBase           = new Tunable("NodeTmBase", 150, 100, 200, 10);
     public final Tunable nodeTmScale          = new Tunable("NodeTmScale", 135, 100, 200, 10);
 
-    public int[][] lmrReductions;
+    public int[][][] lmrReductions;
 
     public Set<Tunable> getTunables() {
         return Set.of(
                 aspMargin, aspFailMargin, aspMaxReduction, nmpDepth, fpDepth, rfpDepth, lmrDepth, lmrBase, lmrDivisor,
-                lmrMinMoves, lmrMinPvMoves, lmpDepth, lmpMultiplier, iirDepth, nmpMargin, nmpImpMargin, nmpBase, nmpDivisor,
-                dpMargin, qsFpMargin, qsSeeEqualDepth, fpMargin, fpScale, rfpMargin, rfpImpMargin, razorDepth, razorMargin,
-                hpMaxDepth, hpMargin, hpOffset, quietHistBonusMax, quietHistBonusScale, quietHistMalusMax, quietHistMalusScale,
-                quietHistMaxScore, captHistBonusMax, captHistBonusScale, captHistMalusMax, captHistMalusScale, captHistMaxScore,
-                contHistBonusMax, contHistBonusScale, contHistMalusMax, contHistMalusScale, contHistMaxScore, nodeTmMinDepth,
-                nodeTmBase, nodeTmScale
+                lmrCapBase, lmrCapDivisor, lmrMinMoves, lmrMinPvMoves, lmpDepth, lmpMultiplier, iirDepth, nmpMargin,
+                nmpImpMargin, nmpBase, nmpDivisor, dpMargin, qsFpMargin, qsSeeEqualDepth, fpMargin, fpScale, rfpMargin,
+                rfpImpMargin, razorDepth, razorMargin, hpMaxDepth, hpMargin, hpOffset, quietHistBonusMax, quietHistBonusScale,
+                quietHistMalusMax, quietHistMalusScale, quietHistMaxScore, captHistBonusMax, captHistBonusScale,
+                captHistMalusMax, captHistMalusScale, captHistMaxScore, contHistBonusMax, contHistBonusScale,
+                contHistMalusMax, contHistMalusScale, contHistMaxScore, nodeTmMinDepth, nodeTmBase, nodeTmScale
         );
     }
 
@@ -110,25 +112,34 @@ public class EngineConfig {
             UCI.write("info error value " + value + " is out of range for option " + name);
         }
         option.value = value;
-        if (name.equals("LmrBase") || name.equals("LmrDivisor")) {
-            calculateLmrReductions();
+        if (name.equals("LmrBase") || name.equals("LmrDivisor")
+                || name.equals("LmrCapBase") || name.equals("LmrCapDivisor")) {
+            calculateLmrTable();
         }
 
         UCI.write("info string " + name + " " + value);
     }
 
     public void postInitialise() {
-        calculateLmrReductions();
+        calculateLmrTable();
     }
 
-    private void calculateLmrReductions() {
-        float lmrBaseFloat = (float) lmrBase.value / 100;
-        float lmrDivisorFloat = (float) lmrDivisor.value / 100;
-        lmrReductions = new int[Search.MAX_DEPTH][];
-        for (int depth = 1; depth < Search.MAX_DEPTH; ++depth) {
-            lmrReductions[depth] = new int[250];
-            for (int movesSearched = 1; movesSearched < 250; ++movesSearched) {
-                lmrReductions[depth][movesSearched] = (int) Math.round(lmrBaseFloat + (Math.log(movesSearched) * Math.log(depth) / lmrDivisorFloat));
+    private void calculateLmrTable() {
+        float quietBase = (float) lmrBase.value / 100;
+        float quietDivisor = (float) lmrDivisor.value / 100;
+        float capBase = (float) lmrCapBase.value / 100;
+        float capDivisor = (float) lmrCapDivisor.value / 100;
+        lmrReductions = new int[2][][];
+
+        for (int quiet = 0; quiet < 2; quiet++) {
+            lmrReductions[quiet] = new int[Search.MAX_DEPTH][];
+            for (int depth = 1; depth < Search.MAX_DEPTH; ++depth) {
+                lmrReductions[quiet][depth] = new int[250];
+                float base = quiet == 0 ? quietBase : capBase;
+                float divisor = quiet == 0 ? quietDivisor : capDivisor;
+                for (int movesSearched = 1; movesSearched < 250; ++movesSearched) {
+                    lmrReductions[quiet][depth][movesSearched] = (int) Math.round(base + (Math.log(movesSearched) * Math.log(depth) / divisor));
+                }
             }
         }
     }

ba09ccfad3be070af565031fd57e94bfc7753ae5
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-10-16 20:37:17 +0200

    Remove test

--- src/test/java/com/kelseyde/calvin/search/picker/MovePickerTest.java
+++ src/test/java/com/kelseyde/calvin/search/picker/MovePickerTest.java
@@ -35,28 +35,4 @@ public class MovePickerTest {
         Assertions.assertEquals(5, moves.size());
     }
 
-    @Test
-    public void testKillersTriedBeforeBadNoisies() {
-
-        Board board = Board.from("rnb1k1nr/pppp1pp1/4p3/4b1qp/4P3/P1N3P1/1PPP1PBP/R1BQK1NR w KQkq - 0 6");
-
-        SearchHistory history = new SearchHistory(new EngineConfig());
-        history.getKillerTable().add(0, Move.fromUCI("d2d4", Move.PAWN_DOUBLE_MOVE_FLAG));
-        Move ttMove = Move.fromUCI("d1g4");
-
-        List<Move> triedMoves = new ArrayList<>();
-
-        MovePicker picker = new MovePicker(moveGenerator, new SearchStack(), history, board, 0, ttMove, false);
-        while (true) {
-            ScoredMove move = picker.pickNextMove();
-            if (move == null) break;
-            triedMoves.add(move.move());
-        }
-        Assertions.assertEquals(30, triedMoves.size());
-        Assertions.assertEquals(Move.fromUCI("d1g4"), triedMoves.get(0));
-        Assertions.assertEquals(Move.fromUCI("d2d4", Move.PAWN_DOUBLE_MOVE_FLAG), triedMoves.get(1));
-        Assertions.assertEquals(Move.fromUCI("d1h5"), triedMoves.get(2));
-
-    }
-
 }

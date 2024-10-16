c5a06361788fa08e522348364f4f86c1e78979f2
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-10-16 19:20:00 +0200

    Add helper methods

--- src/main/java/com/kelseyde/calvin/search/picker/ScoredMove.java
+++ src/main/java/com/kelseyde/calvin/search/picker/ScoredMove.java
@@ -8,5 +8,26 @@ public record ScoredMove(Move move,
                          Piece captured,
                          int score,
                          int historyScore,
-                         MoveType moveType)
-{}
+                         MoveType moveType) {
+
+    public boolean isNoisy() {
+        return moveType == MoveType.GOOD_NOISY || moveType == MoveType.BAD_NOISY;
+    }
+
+    public boolean isGoodNoisy() {
+        return moveType == MoveType.GOOD_NOISY;
+    }
+
+    public boolean isKiller() {
+        return moveType == MoveType.KILLER;
+    }
+
+    public boolean isBadNoisy() {
+        return moveType == MoveType.BAD_NOISY;
+    }
+
+    public boolean isQuiet() {
+        return moveType == MoveType.QUIET;
+    }
+
+}

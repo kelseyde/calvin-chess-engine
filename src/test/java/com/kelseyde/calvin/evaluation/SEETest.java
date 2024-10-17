749d8d7c99b711954a1d975caae36591a8231aa2
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-10-17 20:49:46 +0200

    Update stuff

--- src/test/java/com/kelseyde/calvin/evaluation/SEETest.java
+++ src/test/java/com/kelseyde/calvin/evaluation/SEETest.java
@@ -2,7 +2,7 @@ package com.kelseyde.calvin.evaluation;
 
 import com.kelseyde.calvin.board.Board;
 import com.kelseyde.calvin.board.Move;
-import com.kelseyde.calvin.search.moveordering.SEE;
+import com.kelseyde.calvin.search.SEE;
 import com.kelseyde.calvin.utils.notation.FEN;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;

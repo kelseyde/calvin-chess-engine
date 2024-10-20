580585564ffd51e470c16a159879498736afde60
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-10-20 21:32:12 +0200

    Try with threathist for captures too

--- src/main/java/com/kelseyde/calvin/tables/history/CaptureHistoryTable.java
+++ src/main/java/com/kelseyde/calvin/tables/history/CaptureHistoryTable.java
@@ -2,12 +2,13 @@ package com.kelseyde.calvin.tables.history;
 
 import com.kelseyde.calvin.board.Bits.Square;
 import com.kelseyde.calvin.board.Colour;
+import com.kelseyde.calvin.board.Move;
 import com.kelseyde.calvin.board.Piece;
 import com.kelseyde.calvin.engine.EngineConfig;
 
 public class CaptureHistoryTable extends AbstractHistoryTable {
 
-    int[][][][] table = new int[2][Piece.COUNT][Square.COUNT][Piece.COUNT];
+    int[][][][][] table = new int[2][Piece.COUNT][Square.COUNT][Piece.COUNT][4];
 
     public CaptureHistoryTable(EngineConfig config) {
         super(config.captHistBonusMax.value,
@@ -17,25 +18,31 @@ public class CaptureHistoryTable extends AbstractHistoryTable {
                 config.captHistMaxScore.value);
     }
 
-    public void update(Piece piece, int to, Piece captured, int depth, boolean white, boolean good) {
+    public void update(Move move, Piece piece, Piece captured, long threats, int depth, boolean white, boolean good) {
         int colourIndex = Colour.index(white);
         int pieceIndex = piece.index();
         int capturedIndex = captured.index();
-        int current = table[colourIndex][pieceIndex][to][capturedIndex];
+        int from = move.from();
+        int to = move.to();
+        int threatIndex = threatIndex(from, to, threats);
+        int current = table[colourIndex][pieceIndex][to][capturedIndex][threatIndex];
         int bonus = good ? bonus(depth) : malus(depth);
         int update = gravity(current, bonus);
-        table[colourIndex][pieceIndex][to][capturedIndex] = update;
+        table[colourIndex][pieceIndex][to][capturedIndex][threatIndex] = update;
     }
 
-    public int get(Piece piece, int to, Piece captured, boolean white) {
+    public int get(Move move, Piece piece, Piece captured, long threats, boolean white) {
         int colourIndex = Colour.index(white);
         int pieceIndex = piece.index();
         int capturedIndex = captured.index();
-        return table[colourIndex][pieceIndex][to][capturedIndex];
+        int from = move.from();
+        int to = move.to();
+        int threatIndex = threatIndex(from, to, threats);
+        return table[colourIndex][pieceIndex][to][capturedIndex][threatIndex];
     }
 
     public void clear() {
-        table = new int[2][Piece.COUNT][Square.COUNT][Piece.COUNT];
+        table = new int[2][Piece.COUNT][Square.COUNT][Piece.COUNT][4];
     }
 
 }

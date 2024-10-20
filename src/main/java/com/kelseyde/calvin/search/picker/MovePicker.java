4d352152b20c3c3db95e6441c0518af23d560ba0
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-10-20 13:01:52 +0200

    Stop calling movegen twice in check

--- src/main/java/com/kelseyde/calvin/search/picker/MovePicker.java
+++ src/main/java/com/kelseyde/calvin/search/picker/MovePicker.java
@@ -105,6 +105,7 @@ public class MovePicker {
         if (inCheck) {
             filter = MoveFilter.ALL;
             if (moves != null) {
+                stage = nextStage;
                 return null;
             }
         }

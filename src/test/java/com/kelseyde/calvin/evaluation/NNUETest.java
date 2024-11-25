acff5796f393b62a125d8711961c9c1dc64b7ef8
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-11-25 09:47:46 +0100

    Fix bug with chess960 global var in tests

--- src/test/java/com/kelseyde/calvin/evaluation/NNUETest.java
+++ src/test/java/com/kelseyde/calvin/evaluation/NNUETest.java
@@ -5,6 +5,7 @@ import com.kelseyde.calvin.board.Move;
 import com.kelseyde.calvin.movegen.MoveGenerator;
 import com.kelseyde.calvin.uci.UCI;
 import com.kelseyde.calvin.utils.notation.FEN;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -223,6 +224,11 @@ public class NNUETest {
             board.unmakeMove();
             nnue.unmakeMove();
         }
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        UCI.Options.chess960 = false;
     }
 
 }

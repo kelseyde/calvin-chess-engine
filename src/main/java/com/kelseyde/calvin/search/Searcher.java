3742153744fc11dc9a87bf4a9264d05125744b67
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-01-26 13:15:48 +0100

    Update LMR description

--- src/main/java/com/kelseyde/calvin/search/Searcher.java
+++ src/main/java/com/kelseyde/calvin/search/Searcher.java
@@ -401,8 +401,7 @@ public class Searcher implements Search {
             }
 
             // Late Move Reductions - https://www.chessprogramming.org/Late_Move_Reductions
-            // If the move is ordered late in the list, and isn't a 'noisy' move like a check, capture or promotion,
-            // let's save time by assuming it's less likely to be good, and reduce the search depth.
+            // Moves ordered late in the list are less likely to be good, so we reduce the search depth.
             final int lmrMinMoves = (pvNode ? config.lmrMinPvMoves.value : config.lmrMinMoves.value) + (rootNode ? 1 : 0);
             if (depth >= config.lmrDepth.value && movesSearched >= lmrMinMoves) {
 

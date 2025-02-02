8eadde4378a02ac43e478a0b95bf5bdb1602be01
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-02-02 12:47:35 +0100

    Make OB SPSA params printable

--- src/main/java/com/kelseyde/calvin/engine/EngineConfig.java
+++ src/main/java/com/kelseyde/calvin/engine/EngineConfig.java
@@ -196,6 +196,12 @@ public class EngineConfig {
             return String.format("option name %s type spin default %d min %d max %d", name, value, min, max);
         }
 
+        public String toSPSA() {
+            final float spsaStep = (float) Math.max(0.5, Math.round((float) (max - min) / 20));
+            final float learningRate = 0.002f;
+            return String.format("%s, int, %s, %s, %s, %s, %s", name, value, min, max, spsaStep, learningRate);
+        }
+
     }
 
     public int aspMinDepth() {

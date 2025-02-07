65685808666df6fe47f31c41080bee45fa772253
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-02-07 23:10:31 +0100

    Increase corrhist max? bench 4517143

--- src/main/java/com/kelseyde/calvin/tables/correction/CorrectionHistoryTable.java
+++ src/main/java/com/kelseyde/calvin/tables/correction/CorrectionHistoryTable.java
@@ -20,7 +20,7 @@ import com.kelseyde.calvin.tables.tt.TranspositionTable;
 public abstract class CorrectionHistoryTable {
 
     public static final int SCALE = 256;
-    protected static final int MAX = SCALE * 32;
+    protected static final int MAX = SCALE * 64;
 
     /**
      * Compute the new correction based on a weighted sum of old value and the new delta of the score and static eval.

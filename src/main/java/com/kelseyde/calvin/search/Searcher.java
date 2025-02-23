e1989c5144c5166ad7e592c517ea259bd426c83f
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-02-23 13:35:18 +0100

    Just refactor bench 4701210

--- src/main/java/com/kelseyde/calvin/search/Searcher.java
+++ src/main/java/com/kelseyde/calvin/search/Searcher.java
@@ -271,7 +271,9 @@ public class Searcher implements Search {
         else if (!inCheck) {
             // Re-use cached static eval if available. Don't compute static eval while in check.
             rawStaticEval = ttHit ? ttEntry.staticEval() : eval.evaluate();
-            staticEval = history.correctEvaluation(board, ss, ply, rawStaticEval);
+            staticEval = ttMove != null ?
+                    rawStaticEval :
+                    history.correctEvaluation(board, ss, ply, rawStaticEval);
             uncorrectedStaticEval = rawStaticEval;
 
             // If there is no entry in the TT yet, store the static eval for future re-use.
@@ -301,7 +303,7 @@ public class Searcher implements Search {
             if (depth <= config.rfpDepth()
                     && !Score.isMateScore(alpha)
                     && staticEval - futilityMargin >= beta) {
-                    return beta + (staticEval - beta) / 3;
+                return beta + (staticEval - beta) / 3;
             }
 
             // Razoring
@@ -629,7 +631,9 @@ public class Searcher implements Search {
             // If we are not in check, then we have the option to 'stand pat', i.e. decline to continue the capture chain,
             // if the static evaluation of the position is good enough.
             rawStaticEval = ttHit ? ttEntry.staticEval() : eval.evaluate();
-            staticEval = history.correctEvaluation(board, ss, ply, rawStaticEval);
+            staticEval = ttMove != null ?
+                    rawStaticEval :
+                    history.correctEvaluation(board, ss, ply, rawStaticEval);
 
             if (!ttHit)
                 tt.put(board.key(), HashFlag.NONE, 0, 0, null, rawStaticEval, 0);
@@ -671,24 +675,21 @@ public class Searcher implements Search {
             final boolean promotion = move.isPromotion();
             movesSearched++;
 
-            if (!inCheck && capture && !promotion) {
+            // Delta Pruning
+            // Skip captures where the value of the captured piece plus a margin is still below alpha.
+            if (!inCheck && capture && !promotion && staticEval + SEE.value(captured) + config.dpMargin() < alpha)
+                continue;
 
-                // Delta Pruning
-                // Skip captures where the value of the captured piece plus a margin is still below alpha.
-                if (staticEval + SEE.value(captured) + config.dpMargin() < alpha)
-                    continue;
+            // Futility Pruning
+            // Skip captures that don't win material when the static eval is far below alpha.
+            if (capture && futilityScore <= alpha && !SEE.see(board, move, 1))
+                continue;
 
-                // Futility Pruning
-                // Skip captures that don't win material when the static eval is far below alpha.
-                if (futilityScore <= alpha && !SEE.see(board, move, 1))
-                    continue;
+            // SEE Pruning
+            // Skip moves which lose material once all the pieces are swapped off.
+            if (!inCheck && !SEE.see(board, move, config.qsSeeThreshold()))
+                continue;
 
-                // SEE Pruning
-                // Skip moves which lose material once all the pieces are swapped off.
-                if (!SEE.see(board, move, config.qsSeeThreshold()))
-                    continue;
-
-            }
 
             makeMove(move, piece, sse);
 

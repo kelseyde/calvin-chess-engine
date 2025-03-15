bed7f2361a34e5a3889a116017b1cb0ba85ae4a5
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-03-15 01:31:05 +0100

    Cutnode rfp bench 4060555

--- src/main/java/com/kelseyde/calvin/search/Searcher.java
+++ src/main/java/com/kelseyde/calvin/search/Searcher.java
@@ -296,7 +296,7 @@ public class Searcher implements Search {
 
             // Reverse Futility Pruning
             // Skip nodes where the static eval is far above beta and will thus likely result in a fail-high.
-            final int futilityMargin = Math.max(depth - (improving ? 1 : 0), 0) * config.rfpMargin();
+            final int futilityMargin = Math.max(depth - (improving ? 1 : 0), 0) * config.rfpMargin() - (cutNode ? 60 : 0);
             if (depth <= config.rfpDepth()
                     && !Score.isMate(alpha)
                     && staticEval - futilityMargin >= beta) {

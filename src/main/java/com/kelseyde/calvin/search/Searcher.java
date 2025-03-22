9177dfc05496e2065c67d7a8f5b5b5a28e2a6a3c
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-03-22 10:45:57 +0100

    Always correct static eval bench 3959494

--- src/main/java/com/kelseyde/calvin/search/Searcher.java
+++ src/main/java/com/kelseyde/calvin/search/Searcher.java
@@ -273,7 +273,7 @@ public class Searcher implements Search {
         else if (!inCheck) {
             // Re-use cached static eval if available. Don't compute static eval while in check.
             rawStaticEval = ttHit ? ttEntry.staticEval() : eval.evaluate();
-            staticEval = ttMove != null ? rawStaticEval : history.correctEvaluation(board, ss, ply, rawStaticEval);
+            staticEval = history.correctEvaluation(board, ss, ply, rawStaticEval);
             uncorrectedStaticEval = rawStaticEval;
 
             // If there is no entry in the TT yet, store the static eval for future re-use.
@@ -651,7 +651,7 @@ public class Searcher implements Search {
             // If we are not in check, then we have the option to 'stand pat', i.e. decline to continue the capture chain,
             // if the static evaluation of the position is good enough.
             rawStaticEval = ttHit ? ttEntry.staticEval() : eval.evaluate();
-            staticEval = ttMove != null ? rawStaticEval : history.correctEvaluation(board, ss, ply, rawStaticEval);
+            staticEval = history.correctEvaluation(board, ss, ply, rawStaticEval);
 
             if (!ttHit)
                 tt.put(board.key(), HashFlag.NONE, 0, 0, null, rawStaticEval, 0);

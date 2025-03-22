26202b703a4a7acb9934f46d992e112c3a01d974
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-03-22 21:10:26 +0100

    TT fail low reductions bench 3639842

--- src/main/java/com/kelseyde/calvin/search/Searcher.java
+++ src/main/java/com/kelseyde/calvin/search/Searcher.java
@@ -249,6 +249,14 @@ public class Searcher implements Search {
                 return ttEntry.score();
         }
 
+        if (ttHit
+                && !pvNode
+                && ttEntry.flag() == HashFlag.UPPER
+                && isWithinBounds(ttEntry, alpha, beta)
+                && !isSufficientDepth(ttEntry, depth - 2)) {
+            --depth;
+        }
+
         // Internal Iterative Deepening
         // If the position has not been searched yet, the search will be potentially expensive. So let's search with a
         // reduced depth expecting to record a move that we can use later for a full-depth search.

53748e27a1fd83c32adcd2aa43251f798a8bf83d
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-04-22 20:34:03 +0200

    Remove system out bench 5945241

--- src/main/java/com/kelseyde/calvin/Application.java
+++ src/main/java/com/kelseyde/calvin/Application.java
@@ -5,7 +5,6 @@ import com.kelseyde.calvin.uci.UCI;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Args: " + String.join(", ", args));
         UCI.run(args);
     }
 

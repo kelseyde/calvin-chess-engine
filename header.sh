78e08c65e2be8327d08e894d8d528b652a348f6b
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-04-22 19:34:47 +0200

    Add bench bench 5945241

--- header.sh
+++ header.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-exec java --add-modules=jdk.incubator.vector -jar "$0" "$@"
+exec java --add-modules=jdk.incubator.vector -XX:+UseParallelGC -cp "$0" com.kelseyde.calvin.Application "$@"
+
+exit 1
--- src/main/java/com/kelseyde/calvin/Application.java
+++ src/main/java/com/kelseyde/calvin/Application.java
@@ -5,6 +5,7 @@ import com.kelseyde.calvin.uci.UCI;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("Args: " + String.join(", ", args));
         UCI.run(args);
     }
 

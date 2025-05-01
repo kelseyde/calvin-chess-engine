8a58c0f107667dcb3399e7a7d05a8438df795de1
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-05-01 15:14:04 +0200

    WIP

---
+++ src/main/java/com/kelseyde/calvin/datagen/dataformat/BulletFormat.java
@@ -0,0 +1,15 @@
+package com.kelseyde.calvin.datagen.dataformat;
+
+public class BulletFormat {
+
+    public class ChessBoard {
+        public long occ;
+        public byte[] pcs;
+        public short score;
+        public byte result;
+        public byte ourKing;
+        public byte theirKing;
+        public byte[] extra;
+    }
+
+}
